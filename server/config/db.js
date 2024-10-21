e5b922f0f3f4f348e27a5351efbf098c77a39e50
rvsomthing <77224930+rvsomething@users.noreply.github.com>
2024-10-21 19:15:04 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ const connectToDb = () => {
   mongoose
     .connect("mongodb+srv://ratikvohra:GlobalizationClothing@cluster1.ksgzt64.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1", {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useUnifiedTopology: true,
     })
     .then((data) =>
       console.log(`Database connected with ${data.connection.host}`)
