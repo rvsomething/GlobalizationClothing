f17eeeb6fcca91188562c075b0bc53ac1e18f6aa
rvsomthing <77224930+rvsomething@users.noreply.github.com>
2024-10-21 19:50:29 +0530

    Update constants.js

--- Frontnend/src/utils/constants.js
+++ Frontnend/src/utils/constants.js
@@ -111,12 +111,12 @@ export const services = [
   },
 ];
 
-const domain = process.env.REACT_APP_BACKEND_HOST;
-export const products_url = `http://localhost:5000/api/products`;
-export const single_product_url = `http://localhost:5000/api/products/`;
-export const create_order_url = `http://localhost:5000/api/orders/new`;
-export const get_order_url = `http://localhost:5000/api/orders`;
-export const payment_url = `http://localhost:5000/api/payment/create-payment-intent`;
-export const upload_url = `http://localhost:5000/api/upload/`;
+const domain = process.env.REACT_APP_BACKEND_HOST|| 'http://localhost:5000'; 
+export const products_url = `${domain}/api/products`;
+export const single_product_url = `${domain}/api/products/`;
+export const create_order_url = `${domain}/api/orders/new`;
+export const get_order_url = `${domain}/api/orders`;
+export const payment_url = `${domain}/api/payment/create-payment-intent`;
+export const upload_url = `${domain}/api/upload/`;
 export const default_profile_image =
   'https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg';
