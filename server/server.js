8e4aed0d3add86f0a197bb2eefc1c94e25f4d75a
rvsomthing <77224930+rvsomething@users.noreply.github.com>
2024-10-27 16:33:57 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
   'https://globalizationclothing-frontnend.onrender.com',
-  'https://globalizationclothing-admin.onrender.com,'
+  'https://globalizationclothing-admin.onrender.com,',
 ];
 
 
